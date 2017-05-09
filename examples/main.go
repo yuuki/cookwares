9e3240ff5808cdb1d06f5cfda0a64ae354730fd1
y_uuki <yuki.tsubo@gmail.com>
2017-05-10 00:34:39 +0900

    Update examples

--- examples/main.go
+++ examples/main.go
@@ -13,9 +13,48 @@ func init() {
 
 func main() {
 	log.Println("Starting to cook localhost...")
-
-	if err := aptpackage.Install("nginx", ""); err != nil {
+	if err := run(os.Args); err != nil {
 		log.Println(err)
 		os.Exit(2)
 	}
+}
+
+func run(args []string) error {
+	var err error
+
+	if err = aptpackage.Install("nginx", ""); err != nil {
+		return err
+	}
+
+	// vars := struct {
+	// 	LogFile string
+	// }{
+	// 	"/var/log/nginx/nginx.conf",
+	// }
+	// err = file.Template(&Template{
+	// 	Dest:  "/etc/nginx/nginx.conf",
+	// 	Src:   "templates/nginx.conf.tmpl",
+	// 	Mode:  "0644",
+	// 	Owner: "root",
+	// 	Group: "root",
+	// 	Vars:  vars,
+	// 	Notify: func() error {
+	// 		if err = systemd.Enable("nginx"); err != nil {
+	// 			return err
+	// 		}
+	// 		return systemd.Restart("nginx")
+	// 	},
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// if err = systemd.Enable("nginx"); err != nil {
+	// 	return err
+	// }
+	// if err = systemd.Start("nginx"); err != nil {
+	// 	return err
+	// }
+
+	return nil
 }
